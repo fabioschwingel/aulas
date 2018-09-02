f88a9e8e7db197f54b4e139a043cc86d45912eac
Fabio cesar <fabio_schwingel@hotmail.com>
2018-09-02 14:58:04 +0000

    aula domingo

---
+++ puppetlabs/code/environments/production/manifests/site.pp
@@ -0,0 +1,16 @@
+node "default" {
+
+	include capuz_vermelho
+
+}
+
+node /(devops|automation).salas4linux.com.br/ {
+
+	include base
+}
+
+node docker.salas4linux.com.br {
+
+	include mr_miracle
+	include docker
+}
